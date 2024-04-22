9a8e528e4b862c03e208303798b34ecb09457812
SilviaUvA <silviaabbring@gmail.com>
2024-04-22 13:23:37 +0200

    python 3.10 env

--- install_env.sh
+++ install_env.sh
@@ -15,7 +15,8 @@ module load Anaconda3/2022.05
 source ~/.bashrc
 cd $HOME/CEConvDL2/CEConv
 
-conda create -n CEConv python=3.12 pip
+# conda env remove --name CEConv
+conda create -n CEConv python=3.10 pip
 conda activate CEConv
 
 pip install -r requirements.txt
