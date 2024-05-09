de9e0e42425bf033e6b36f5b5fd9a9386cf548b7
SilviaUvA <silviaabbring@gmail.com>
2024-05-09 12:42:59 +0200

    hsv_space, hue_shift en sat_shift toegevoegd

--- CEConv/models/resnet_hybrid.py
+++ CEConv/models/resnet_hybrid.py
@@ -58,7 +58,8 @@ class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(
-        self, in_planes, planes, stride=1, rotations=1, separable=False, lab_space = False,
+        self, in_planes, planes, stride=1, rotations=1, separable=False, lab_space=False,
+        hsv_space=False, sat_shift=False, hue_shift=False
     ) -> None:
         super(BasicBlock, self).__init__()
 
@@ -102,6 +103,9 @@ class BasicBlock(nn.Module):
                 bias=False,
                 separable=separable,
                 lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
             self.conv2 = CEConv2d(
                 rotations,
@@ -114,6 +118,9 @@ class BasicBlock(nn.Module):
                 bias=False,
                 separable=separable,
                 lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
             if stride != 1 or in_planes != self.expansion * planes:
                 self.shortcut = nn.Sequential(
@@ -127,6 +134,9 @@ class BasicBlock(nn.Module):
                         bias=False,
                         separable=False,
                         lab_space = lab_space,
+                        hsv_space=hsv_space,
+                        sat_shift=sat_shift,
+                        hue_shift=hue_shift,
                     ),
                     bnlayer(self.expansion * planes),
                 )
@@ -142,7 +152,8 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1, rotations=1, separable=False, lab_space=False):
+    def __init__(self, in_planes, planes, stride=1, rotations=1, separable=False, lab_space=False,
+                 hsv_space=False, sat_shift=False, hue_shift=False):
         super(Bottleneck, self).__init__()
         bnlayer = nn.BatchNorm2d if rotations == 1 else nn.BatchNorm3d
         self.bn1 = bnlayer(planes)
@@ -181,6 +192,9 @@ class Bottleneck(nn.Module):
                 bias=False,
                 separable=separable,
                 lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
             self.conv2 = CEConv2d(
                 rotations,
@@ -193,6 +207,9 @@ class Bottleneck(nn.Module):
                 bias=False,
                 separable=separable,
                 lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
             self.conv3 = CEConv2d(
                 rotations,
@@ -203,6 +220,9 @@ class Bottleneck(nn.Module):
                 bias=False,
                 separable=separable,
                 lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
 
             if stride != 1 or in_planes != self.expansion * planes:
@@ -217,6 +237,9 @@ class Bottleneck(nn.Module):
                         bias=False,
                         separable=False,
                         lab_space = lab_space,
+                        hsv_space=hsv_space,
+                        sat_shift=sat_shift,
+                        hue_shift=hue_shift,
                     ),
                     bnlayer(self.expansion * planes),
                 )
@@ -259,6 +282,9 @@ class HybridResNet(nn.Module):
         width=64,
         separable=False,
         lab_space=False,
+        hsv_space=False,
+        sat_shift=False,
+        hue_shift=False
     ) -> None:
         super(HybridResNet, self).__init__()
 
@@ -313,7 +339,10 @@ class HybridResNet(nn.Module):
                 bias=False,
                 learnable=learnable,
                 separable=separable,
-                lab_space = lab_space
+                lab_space = lab_space,
+                hsv_space=hsv_space,
+                sat_shift=sat_shift,
+                hue_shift=hue_shift,
             )
             self.bn1 = nn.BatchNorm3d(channels[0])
             if not low_resolution:
@@ -345,6 +374,9 @@ class HybridResNet(nn.Module):
                     rotations=block_rotations,
                     separable=separable,
                     lab_space=lab_space,
+                    hsv_space=hsv_space,
+                    sat_shift=sat_shift,
+                    hue_shift=hue_shift,
                 )
             )
         # Pooling layers
@@ -358,11 +390,12 @@ class HybridResNet(nn.Module):
                 channels[-1] * rotations * block.expansion, num_classes
             )
 
-    def _make_layer(self, block, planes, num_blocks, stride, rotations, separable, lab_space):
+    def _make_layer(self, block, planes, num_blocks, stride, rotations, separable, lab_space, hsv_space, sat_shift, hue_shift):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride, rotations, separable, lab_space=lab_space))
+            layers.append(block(self.in_planes, planes, stride, rotations, separable, lab_space=lab_space, 
+                                hsv_space=hsv_space, sat_shift=sat_shift, hue_shift=hue_shift,))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
