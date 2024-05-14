f25d09899b6238d397dc2c7c5882d18f0a9798c7
Arco <arcovanbreda@gmail.com>
2024-05-14 10:29:39 +0200

    Compatability changes

--- CEConv/experiments/classification/train.py
+++ CEConv/experiments/classification/train.py
@@ -50,9 +50,12 @@ class PL_model(pl.LightningModule):
 
         # Store accuracy metrics for testing.
         self.test_acc_dict = {}
-        self.test_rotations = 37
+        self.test_rotations = 111#37
         self.test_jitter = np.linspace(-0.5, 0.5, self.test_rotations)
-        self.lab_test = args.lab_test
+        if hasattr(self.args, 'lab_test'):
+            self.lab_test = args.lab_test
+        else:
+            self.lab_test = False
         if self.lab_test:
             angle_delta =  2 * math.pi / self.test_rotations
             self.lab_angle_matrix = torch.tensor(
@@ -190,8 +193,8 @@ class PL_model(pl.LightningModule):
                 x = rgb2lab.forward(None,x)
                 
             # Normalize images.
-            if args.normalize:
-                x = normalize(x, grayscale=args.grayscale or args.rotations > 1, lab=True if self.lab else False) #TODO convert to hsv around here
+            if self.args.normalize:
+                x = normalize(x, grayscale=self.args.grayscale or self.args.rotations > 1, lab=True if self.lab else False) #TODO convert to hsv around here
 
             # Forward pass and compute loss.
             y_pred = self.model(x)
