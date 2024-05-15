1ba5918e9409f24e92d8f73dc1af0aab76b2a270
Your Name <you@example.com>
2024-05-15 10:57:49 +0200

    Bugfixes

--- CEConv/ceconv/ceconv2d.py
+++ CEConv/ceconv/ceconv2d.py
@@ -18,8 +18,8 @@ def _trans_input_filter_hsv(weights, out_rotations, hue_shift, sat_shift) -> tor
         hue_shift: Bool, signaling whether or not to perform a hue shift on the filter
         sat_shift: Bool, signaling whether or not to perform a saturation shift on the filter
     """
-    if not hue_shift and not sat_shift and out_rotations < 2:
-        raise ValueError("Flag hue_shift, sat_shift or both -> Cant be left empty in hsv_space with more than identity rotation")
+    if not hue_shift and not sat_shift:
+        raise ValueError("Flag hue_shift, sat_shift or both -> Cant be left empty in hsv_space")
     if sat_shift:
         neg_sats = out_rotations // 2
         pos_sats = neg_sats - 1 + out_rotations % 2
@@ -113,7 +113,6 @@ def _shifted_img_stack(imgs, out_rotations, hue_shift, sat_shift, val_shift):
         neg_sats = out_rotations // 2
         pos_sats = neg_sats - 1 + out_rotations % 2
         val_shifts = np.append(np.linspace(-0.5, 0, neg_sats+1)[:-1], np.linspace(0, 1, pos_sats+1))
-
     for i in range(out_rotations):
         imgs_cloned = imgs.clone()
         if hue_shift:
